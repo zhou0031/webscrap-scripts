6841745dbcb3ae876cb92d44e58dbff638848b50
ryan <ryan.z.williams@gmail.com>
2023-10-13 23:08:32 +0800

    nytime content

--- nytimes.py
+++ nytimes.py
@@ -9,23 +9,33 @@ def parse_and_print_content(link):
     try:
         response = requests.get(link)
         if response.status_code == 200:
+            content = []
             soup = BeautifulSoup(response.content, 'html.parser')
             # Extract and print the content (modify selectors accordingly based on the website's structure)
-            
-            header = soup.find('div',class_='article-header')
-            #content = soup.find(class_=re.compile("index_main_content.*")).text.strip()
-            #images=soup.find_all("img",{'data-lazyload':re.compile(".*")})
-            print("article-header:", header)
-            #print("Content:", content)
-           
-            #for img in images:
-            #	print(img['data-lazyload'])
-            	
+
+            article_header = soup.find(
+                'div', class_='article-header').find('header').text.strip()
+
+            paragraphs = soup.find_all(
+                'div', class_='article-paragraph')  # .text.strip()
+            for p in paragraphs:
+                if p.find('figure'):
+                    continue
+                content.append(p.text.strip())
+
+            # images=soup.find_all("img",{'data-lazyload':re.compile(".*")})
+            print("Title:", article_header)
+            print("Content:", content)
+
+            # for img in images:
+            # print(img['data-lazyload'])
+
             print("-" * 30)
         else:
             print("Failed to retrieve the page:", link)
     except requests.exceptions.RequestException as e:
         print("Error:", e)
+
 
 def parse_news(url):
     try:
@@ -37,17 +47,19 @@ def parse_news(url):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Find all h3 elements
-        li_elements = soup.find_all('li',{'data-tb-region-item':re.compile('.*')})
+        li_elements = soup.find_all(
+            'li', {'data-tb-region-item': re.compile('.*')})
 
         for li in li_elements:
-            
+
             link = li.find('a')
-            href=urljoin(url,link['href'])
-            	
+            href = urljoin(url, link['href'])
+
             if link:
-               parse_and_print_content(href)
+                parse_and_print_content(href)
     except requests.exceptions.RequestException as e:
         print("Error:", e)
+
 
 # Example usage
 start_url = 'https://cn.nytimes.com/'
